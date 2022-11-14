3f794fa41a8d576fad1eec10604881d453db9422
Jay Collett <hello@jaycollett.co>
2022-11-14 23:45:35 +0000

    build error

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
 // pages/_document.js
-import CustomHead from '@/components/_Layout/CustomHead'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html className="bg-neutral-500">
       <Head>
-        <CustomHead />
       </Head>
       <body>
         <Main />
