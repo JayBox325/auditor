28cc108f5329fa17e41789007e3b9995069ef365
Jay Collett <hello@jaycollett.co>
2022-11-15 10:15:40 +0000

    bg colour

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className="bg-neutral-500">
+    <Html className="bg-neutral-900">
       <Head>
       </Head>
       <body>
